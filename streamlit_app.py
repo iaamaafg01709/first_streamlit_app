e2eaadfb45785e6c8855e1d6832813e248066949
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-06-27 15:07:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.header('Breakfast Favorite')
 streamlit.title(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.title('🥗 Kale,Spinach and Rocket Smoothie')
 streamlit.title(' 🐔 Hard.boiled.free.range Egg')
-streamlit.title('🥑🍞 Avocado and bread'
+streamlit.title('🥑🍞 Avocado and bread')
 
  
