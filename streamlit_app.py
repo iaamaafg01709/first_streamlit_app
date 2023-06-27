6854f14d7701075979b83399b53e70a9fddb1d1d
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-06-27 15:08:58 -0700

    Update streamlit_app.py
    Added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ streamlit.title(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.title('🥗 Kale,Spinach and Rocket Smoothie')
 streamlit.title(' 🐔 Hard.boiled.free.range Egg')
 streamlit.title('🥑🍞 Avocado and bread')
+streamlit.header('Build Your Own Fruit Smoothie')
+
 
  
