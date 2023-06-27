f40543a9d073453ab9f6dfa360f501932974ddb4
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-06-27 14:37:29 -0700

    Update streamlit_app.py
    Added more code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my Parent New health dinner')
+streamlit.header('Breakfast menu')
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kale,Spinach and Rocket Smoothie')
+streamlit.title('Hard.boiled.free.range Egg')
