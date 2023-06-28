da377a3dfaca719ebb2c1f764ea046e658294796
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-06-28 09:10:25 -0700

    Update streamlit_app.py
    Fixed the link

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-my_friut_list=pandas.read_csv("https://uni-lab-files.s3.us-west-s3-us-west-2.amazonws.com/dabw/fruit_macros.txt")
+my_friut_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.header('Breakfast Favorite')
 streamlit.title(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.title('🥗 Kale,Spinach and Rocket Smoothie')
