d9b795bd82af1c0061af63aad09ab08d934b61d0
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-06-28 09:08:51 -0700

    Update streamlit_app.py
    Added import function Python to pull data from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit
+import pandas
+my_friut_list=pandas.read_csv("https://uni-lab-files.s3.us-west-s3-us-west-2.amazonws.com/dabw/fruit_macros.txt")
 streamlit.header('Breakfast Favorite')
 streamlit.title(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.title('🥗 Kale,Spinach and Rocket Smoothie')
 streamlit.title(' 🐔 Hard.boiled.free.range Egg')
 streamlit.title('🥑🍞 Avocado and bread')
 streamlit.header('Build Your Own Fruit Smoothie')
+streamlit.dataframe(my_fruit_list)
 
 
  
