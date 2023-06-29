774c05bf2c1703f61de0f8bd8fa818159f52715a
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-06-28 18:25:13 -0700

    Update streamlit_app.py
    Added the index

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #Let's put pict
+my_fruit_list=my-fruit_list.set_index('Fruit')
 
 streamlit.header('Breakfast Favorite')
 streamlit.title(' 🥣 Omega 3 and Blueberry Oatmeal')
