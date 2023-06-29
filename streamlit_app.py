58703eb19403e001add0e09582d599f8ca0a5a44
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-06-28 18:22:52 -0700

    Update streamlit_app.py
    Added index list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,18 @@
 import streamlit
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Let's put pict
+
 streamlit.header('Breakfast Favorite')
 streamlit.title(' 🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.title('🥗 Kale,Spinach and Rocket Smoothie')
 streamlit.title(' 🐔 Hard.boiled.free.range Egg')
 streamlit.title('🥑🍞 Avocado and bread')
 streamlit.header('Build Your Own Fruit Smoothie')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+#display the table on the page
 
 
  
