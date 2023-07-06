ab1aed0e441f4dedd1e90722d1fa53025aebd4c7
iaamaafg01709 <137946313+iaamaafg01709@users.noreply.github.com>
2023-07-06 11:09:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-#
+#what
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
